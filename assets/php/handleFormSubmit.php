7167d3f0e8cf6007122fd8cb8c2a282deb973a56
Almir Bijedic <almir.b@aquila-ars.com>
2015-05-12 13:35:26 +0200

    added swiftmailer

--- assets/php/handleFormSubmit.php
+++ assets/php/handleFormSubmit.php
@@ -1,10 +1,26 @@
 <?php
-
+require_once '../../vendor/swiftmailer/swiftmailer/lib/swift_required.php';
 if(isset($_POST['c_name'])){
-    
-    $res['sendstatus'] = 1;
-    $res['message'] = 'Form Submission Succesful';
-    echo json_encode($res);
+	$transport = Swift_SmtpTransport::newInstance('ssl://smtp.gmail.com', 465)
+	                                ->setUsername('username@gmail.com') // Your Gmail Username
+	                                ->setPassword('my_secure_gmail_password'); // Your Gmail Password
+	$mailer = Swift_Mailer::newInstance($transport);
+	$message = Swift_Message::newInstance('Wonderful Subject Here')
+	                        ->setFrom(array('sender@example.com' => 'Sender Name')) // can be $_POST['email'] etc...
+	                        ->setTo(array('receiver@example.com' => 'Receiver Name')) // your email / multiple supported.
+	                        ->setBody('Here is the <strong>message</strong> itself. It can be text or <h1>HTML</h1>.', 'text/html');
+
+	// Send the message
+	if ($mailer->send($message)) {
+		$res['sendstatus'] = 1;
+		$res['message'] = 'Form Submission Succesful';
+		echo json_encode($res);
+	} else {
+		$res['sendstatus'] = 1;
+		$res['message'] = 'Mail not sent. Please try manually to almir.bij@gmail.com';
+		echo json_encode($res);
+	}
+
 }
 
 ?>
