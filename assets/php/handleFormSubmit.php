d691fd5606e1f2e80e91e50615300bbf62105738
Almir Bijedic <almir.bij@gmail.com>
2017-10-18 10:28:28 +0200

    fix typo POST_

--- assets/php/handleFormSubmit.php
+++ assets/php/handleFormSubmit.php
@@ -8,7 +8,7 @@ if(isset($_POST['c_name'])){
 	$message = Swift_Message::newInstance('New message from almirbijedic.rocks')
 	                        ->setFrom(array('almir.bij@gmail.com' => 'almirbijedic.rocks')) // can be $_POST['email'] etc...
 	                        ->setTo(array('almir.bij@gmail.com' => 'Almir Bijedic')) // your email / multiple supported.
-	                        ->setBody($POST_['c_message']);
+	                        ->setBody($_POST['c_message']);
 
 	// Send the message
 	if ($mailer->send($message)) {
