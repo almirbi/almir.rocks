c0643e14d9acef2b9cd8d9675f36d00198accb39
Almir Bijedic <almir.bij@gmail.com>
2017-10-21 17:29:01 +0200

    Network first service worker

--- sw.js
+++ sw.js
@@ -63,7 +63,7 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+            return fetch(event.request) || response;
         })
     );
 });
